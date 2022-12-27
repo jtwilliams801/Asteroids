6b63f163704bc3e2ceee143fd889e3c9c5018f04
jtwilliams801 <jtwilliams@gmail.com>
2022-12-27 02:54:26 -0700

    Added asteroids

--- sketch.js
+++ sketch.js
@@ -1,6 +1,8 @@
 var ship;
+var asteroids = [];
 var thrustSound;
 let playMode = 'sustain';
+
 
 
 function preload() {
@@ -11,6 +13,11 @@ function preload() {
 function setup() {
   createCanvas(windowWidth, windowHeight);
   ship = new Ship();
+  for (var i=0; i<10;i++) {
+    asteroids.push(new Asteroid());
+  }
+
+
 }
 
 function draw() {
@@ -19,6 +26,12 @@ function draw() {
   ship.turn();
   ship.update();
   ship.edges();
+
+  for (var i=0; i < asteroids.length; i++) {
+    asteroids[i].render();
+    asteroids[i].update();
+    asteroids[i].edges();
+  }
 }
 
 function keyReleased() {
@@ -39,72 +52,3 @@ function keyPressed() {
 
 }
 
-function Ship() {
-  this.pos = createVector(width/2, height/2)
-  this.r = 20;
-  this.heading = 0;
-  this.rotation = 0;
-  this.vel = createVector(0,0);
-  this.isBoosting = false;
-
-  this.boosting = function(b) {
-    this.isBoosting = b;
-
-  }
-
-
-  this.update = function() {
-    if (this.isBoosting) {
-      this.boost();
-    }
-    this.pos.add(this.vel);
-    this.vel.mult(0.97);
-  }
-
-  this.boost = function() {
-    var force = p5.Vector.fromAngle(this.heading);
-    this.vel.add(force);
-
-    if (!thrustSound.isPlaying()){
-      thrustSound.play();
-    }
-
-    
-    
-    
-  }
-
-  this.render = function() {  
-    noFill();
-    stroke(255);
-   
-    translate(this.pos.x, this.pos.y);
-    rotate(this.heading + PI/2);
-    triangle(-this.r/2, this.r, this.r/2, this.r, 0, -this.r)
-  }
-
-  this.edges = function() {
-    if(this.pos.x > width + this.r) {
-      this.pos.x = -this.r;
-    } else if (this.pos.x < -this.r) {
-      this.pos.x = width + this.r;
-    }
-
-    if(this.pos.y > height + this.r) {
-      this.pos.y = -this.r;
-    } else if (this.pos.y < -this.r) {
-      this.pos.y = height + this.r;
-    }
-  }
-
-  this.setRotation = function(a) {
-    this.rotation = a;
-  }
-
-  this.turn = function(angle) {
-    this.heading += this.rotation;
-  }
-
-
-
-}
