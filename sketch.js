028a7f9fd0bcf4c7d5b7fe228d1548eb13d355e5
jtwilliams801 <jtwilliams@gmail.com>
2022-12-26 22:40:13 -0700

    Added ship and flying

--- sketch.js
+++ sketch.js
@@ -1,14 +1,95 @@
+var ship;
+
+
+
 function setup() {
-  createCanvas(700, 700);
+  createCanvas(windowWidth, windowHeight);
+  ship = new Ship();
 }
 
 function draw() {
-  background(220);
-  if (mouseIsPressed) {
-    fill(0)
+  background(0);
+  ship.render();
+  ship.turn();
+  ship.update();
+  ship.edges();
+}
+
+function keyReleased() {
+  ship.setRotation(0);
+  if (keyCode == UP_ARROW) {
+    ship.boosting(false);
   }
-  else {
-    fill(255)
+}
+
+function keyPressed() {
+  if (keyCode == RIGHT_ARROW) {
+    ship.setRotation(0.1);
+  } else if (keyCode == LEFT_ARROW) {
+    ship.setRotation(-0.1);
+  } else if (keyCode == UP_ARROW) {
+    ship.boosting(true);
   }
-  ellipse(mouseX, mouseY,80,80)
+
+}
+
+function Ship() {
+  this.pos = createVector(width/2, height/2)
+  this.r = 20;
+  this.heading = 0;
+  this.rotation = 0;
+  this.vel = createVector(0,0);
+  this.isBoosting = false;
+
+  this.boosting = function(b) {
+    this.isBoosting = b;
+  }
+
+
+  this.update = function() {
+    if (this.isBoosting) {
+      this.boost();
+    }
+    this.pos.add(this.vel);
+    this.vel.mult(0.95);
+  }
+
+  this.boost = function() {
+    var force = p5.Vector.fromAngle(this.heading);
+    this.vel.add(force);
+  }
+
+  this.render = function() {  
+    noFill();
+    stroke(255);
+   
+    translate(this.pos.x, this.pos.y);
+    rotate(this.heading + PI/2);
+    triangle(-this.r/2, this.r, this.r/2, this.r, 0, -this.r)
+  }
+
+  this.edges = function() {
+    if(this.pos.x > width + this.r) {
+      this.pos.x = -this.r;
+    } else if (this.pos.x < -this.r) {
+      this.pos.x = width + this.r;
+    }
+
+    if(this.pos.y > height + this.r) {
+      this.pos.y = -this.r;
+    } else if (this.pos.y < -this.r) {
+      this.pos.y = height + this.r;
+    }
+  }
+
+  this.setRotation = function(a) {
+    this.rotation = a;
+  }
+
+  this.turn = function(angle) {
+    this.heading += this.rotation;
+  }
+
+
+
 }
